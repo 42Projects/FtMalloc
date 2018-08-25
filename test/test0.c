24b0b86e6077e624f703b265f3ded40c3b8a1b64
Terry Finkel <nfinkel@student.42.fr>
2018-08-25 20:41:48 +0200

    stuff

--- test/test0.c
+++ test/test0.c
@@ -1,32 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   malloc.h                                           :+:      :+:    :+:   */
+/*   test0.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nfinkel <nfinkel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/08/25 16:05:42 by nfinkel           #+#    #+#             */
-/*   Updated: 2018/08/25 20:31:26 by nfinkel          ###   ########.fr       */
+/*   Created: 2018/08/25 20:39:23 by nfinkel           #+#    #+#             */
+/*   Updated: 2018/08/25 20:39:24 by nfinkel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MALLOC_H
-# define MALLOC_H
+int main(void)
+{
+	int		k = -1;
+	char	*addr = (void *)0;
 
-# include <unistd.h>
-# include <stdbool.h>
-# include <sys/mman.h>
-
-# define TINY
-# define SMALL
-# define LARGE
-
-bool		g_mutex;
-
-extern void	free(void *ptr);
-extern void	*malloc(size_t size);
-extern void	*realloc(void *ptr, size_t size);
-extern void	show_alloc_mem(void);
-extern void	show_alloc_mem_ex(void);
-
-#endif
+	while (++k < 1024);
+	return 0;
+}
